7dbc6fc187152a321e15398c126e366dc53c83b1
Tama McGlinn <t.mcglinn@gmail.com>
2016-05-15 23:36:02 +0200

    Added fully compile time fibonacci calculation

--- FuncTest/FuncTest.cpp
+++ FuncTest/FuncTest.cpp
@@ -10,8 +10,21 @@ int fib(int n) {
 	return cached_function(fib, n - 1) + cached_function(fib, n - 2);
 }
 
+// same fibonacci function, but fully at compile time
+
+template< size_t N >
+struct compile_fib : std::integral_constant<size_t, compile_fib<N-1>::value + compile_fib<N-2>::value > {};
+
+template<>
+struct compile_fib<1> : std::integral_constant<size_t, 1> {};
+
+template<>
+struct compile_fib<2> : std::integral_constant<size_t, 1> {};
+
 int main()
 {
+	printf("%i = %i\n", 44, compile_fib<44>::value);
+
 	for (int i = 0; i < 45; ++i)
 	{
 		printf("%i = %i\n", i, fib(i));
