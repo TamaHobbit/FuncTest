17a9df60c77ef0ef7f4da0821a057b97d4926dee
Tama McGlinn <t.mcglinn@gmail.com>
2016-05-15 16:01:59 +0200

    moved cached function into seperate header and namespace

--- FuncTest/FuncTest.cpp
+++ FuncTest/FuncTest.cpp
@@ -1,37 +1,8 @@
 // FuncTest.cpp : Defines the entry point for the console application.
 //
 
-#include <map>
-#include <tuple>
-
-// http://stackoverflow.com/questions/28509273/get-types-of-function-parameters
-//#include <boost/function_types/function_type.hpp>
-//#include <boost/function_types/parameter_types.hpp>
-#include <boost/function_types/result_type.hpp>
-
-template <typename FuncType>
-using ResultType = typename boost::function_types::result_type<FuncType>::type;
-
-// based on http://madebyevan.com/obscure-cpp-features/ but extended to not hardcode the return and parameter types
-
-template <class FuncType, FuncType functionPointer, class SoleArgType>
-ResultType<FuncType> cached_function(SoleArgType argument) {
-	static std::map< SoleArgType, ResultType<FuncType> > cache;
-	auto cacheIterator = cache.find(argument);
-	if (cacheIterator != cache.end()) return cacheIterator->second;
-	return cache[argument] = functionPointer(argument);
-}
-
-template <class FuncType, class ... ArgTypes>
-ResultType<FuncType> cached_function(FuncType functionPointer, ArgTypes ... arguments) {
-	// a tuple type to map all our function's parameters to the return type
-	using ArgumentTupleType = std::tuple<ArgTypes...>;
-	ArgumentTupleType parameters = std::make_tuple(arguments...);
-	static std::map< ArgumentTupleType, ResultType<FuncType> > cache;
-	auto cacheIterator = cache.find(parameters);
-	if (cacheIterator != cache.end()) return cacheIterator->second;
-	return cache[parameters] = functionPointer(arguments...);
-}
+#include "cached_function.hpp"
+using namespace CachedFunction;
 
 int fib(int n) {
 	if (n < 2) return n;
